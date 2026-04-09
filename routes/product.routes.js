26dba658539e9f88364411717a4871d53cf35f17
Aryan Mishra <aryan456.rehla@gmail.com>
2026-04-10 01:10:22 +0530

    GetProduct api made

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { postProduct } from '../controller/product.contoller.js';
+import { postProduct } from '../controller/newProduct.controller';
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRoutes from './routes/user.routes.js';
 import productRoutes from './routes/product.routes.js';
 import purchaseRoutes from './routes/purchase.routes.js'
+import getProductRoutes from './routes/GetProducts.routes.js'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +20,7 @@ app.get('/health', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/purchases', purchaseRoutes)
+app.use('/api/products', getProductRoutes)
 
 app.listen(PORT, () => {
     console.log(`\n✅ Server running at http://localhost:${PORT}`);
